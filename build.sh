6c035c9059c69922a9efe98c6b4d208ed3a4379e
Arunkumar Dhananjayan <arunkd13@gmail.com>
2025-04-27 06:04:26 +0530

    Creating cover and standard book names

--- build.sh
+++
@@ -1,9 +0,0 @@
-F="Precalculus4-print.tex Precalculus4-ebook.tex"
-
-for f in $F ; do
-
-pdflatex $f
-for diagram in $(basename -s .mp $(find . -name '*.mp')) ; do mpost $diagram ; done
-pdflatex $f
-
-done
